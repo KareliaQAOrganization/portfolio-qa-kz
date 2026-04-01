49452ad893f0b4f66ef5e10c82fc6b8b2eb8c9a7
Karelia Zamorano <karelia.zamorano@gmail.com>
2026-04-01 21:04:13 +0200

    Update update.sql

--- sql/update.sql
+++ sql/update.sql
@@ -1,10 +1,11 @@
 -- Cambiar el email de laura por lauratest@gmail.com
 UPDATE form
-SET email = 'contact@gmail.com'
-WHERE id = 12;
+SET email = 'lauratest@gmail.com'
+WHERE name = 'laura';
 
 
 -- Cambiar edad 20 por 21.
 UPDATE form
 SET age = 21
 WHERE age = 20;
+
