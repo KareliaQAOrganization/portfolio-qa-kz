e0e5122fde992350527b690cc98e80e562367120
Karelia Zamorano <karelia.zamorano@gmail.com>
2026-04-01 02:02:08 +0200

    Update update.sql

---
+++ sql/update.sql
@@ -0,0 +1,10 @@
+-- Cambiar el email de laura por lauratest@gmail.com
+UPDATE form
+SET email = 'contact@gmail.com'
+WHERE id = 12;
+
+
+-- Cambiar edad 20 por 21.
+UPDATE form
+SET age = 21
+WHERE age = 20;
